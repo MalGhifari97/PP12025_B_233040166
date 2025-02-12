1c09a8cead330350f45cf232d69bc52e75e3661e
Mal_Ghifari97 <akmal.233040166@mail.unpas.ac.id>
2025-02-12 11:52:05 +0700

    first commit

---
+++ P1/Node.java
@@ -0,0 +1,29 @@
+package P1;
+
+public class Node {
+	private int Nilai;
+	private Node Next;
+	
+	//inisialisasi Node
+	public Node(int nilai) {
+		this.Nilai = nilai;
+	}
+	
+	public void setNilai (int Nilai) {
+		this.Nilai=Nilai;
+	}
+	
+	public int getNilai() {
+		return Nilai;
+	}
+	
+	public void setNext (Node Next) {
+		this.Next = Next;
+	}
+	
+	public Node getNext() {
+		return Next;
+	}
+
+	
+}
---
+++ P1/NodeMain.java
@@ -0,0 +1,42 @@
+package P1;
+
+public class NodeMain {
+	public static void main(String[] args) {
+		//membuat 6 buah node n1,n2,n3,n4,n5 & n6
+		Node n1 = new Node (5);
+		Node n2 = new Node (7);
+		Node n3 = new Node (9);
+		Node n4 = new Node (8);
+		Node n5 = new Node (2);
+		Node n6 = new Node (3);
+		
+		
+		//membuat relasi Node n1,n2,n3,n4,n5, & n6
+		n1.setNext(n2);
+		
+		n2.setNext(n3);
+		
+		n3.setNext(n4);
+		
+		n5.setNext(n6);
+		
+		n6.setNext(n1);
+		
+		//menampilkan Node n1,n2,n3,n4,n5 & n6 dengan pointer p
+		Node p = n1;
+		Node p1 = n5;
+		while(p != null)
+		{
+			System.out.printf("%d ", p.getNilai());
+			p = p.getNext();
+		}
+		System.out.println("");
+		while(p1 != n4)
+		{
+			System.out.printf("%d ", p1.getNilai());
+			p1 = p1.getNext();
+		}
+	}
+
+}
+
