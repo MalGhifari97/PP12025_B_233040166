6d7c93847198335d8b7cd2d929202319f29e0b67
Mal_Ghifari97 <145556126+MalGhifari97@users.noreply.github.com>
2025-04-30 11:10:02 +0700

    Add files via upload

---
+++ p7/StrukturListTest.java
@@ -0,0 +1,19 @@
+package p7;
+
+public class StrukturListTest {
+	 public static void main(String[] args) {
+	       
+	      
+	        StrukturList list3 = new StrukturList();
+	        list3.addTail(4);
+	        list3.addTail(3);
+	        list3.addTail(7);
+	        list3.addTail(8);
+	        list3.addTail(1);
+	        list3.addTail(9);
+	        list3.display();
+	        list3.removeALL();
+	        list3.display();
+	        
+	    }
+}
